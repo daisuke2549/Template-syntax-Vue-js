92758b82b2950b41079cf0f0403b527173eea9f0
daisuke sasaki <gerund25495641@gmail.com>
2021-03-05 15:22:32 +0900

    テンプレート構文学習終了(まだ完璧ではないga)

--- js/main.js
+++ js/main.js
@@ -27,3 +27,89 @@ var app = new Vue({
         message: '  Hello <span style="color:red;"> Vue.js</span>'
      }
 })
+
+
+/* v-cloakディレクティブ */
+var app = new Vue({
+   el:'#app4',
+   data:{
+      message: 'Hello Vue.js'
+   }
+})
+
+
+/* v-textディレクティブ ⇨基本的にはMustache構文に統一するのがおすすめ*/
+var app = new Vue({
+   el:'#app5',
+   data:{
+      message: 'Hello Vue.js'
+   }
+})
+
+
+/* バインディング式 */
+/* javascript式 */
+
+// var app = new Vue({
+//    el:'#app6',
+//    data:{
+//       message: 'Hello Vue.js',
+//       number:100,
+//       ok:false
+//    }
+// })
+
+
+
+/* フィルタ式*/
+
+
+/* グローバルフィルター*/
+
+// Vue.filter('numberFormat', function(value){
+//      return value.toLocaleString();
+// })
+
+
+
+// var app = new Vue({
+//    el:'#app7',
+//    data:{
+//       price: 2980000000
+//    },
+//    // filters:{
+//    //    numberFormat: function(value){
+//    //       return value.toLocaleString()
+//    //    }
+//    // }
+// })
+
+
+
+// Vue.filter('toUSD', function(jpy){
+//    return jpy /  100
+// }
+
+
+// Vue.filter('numberFormat', function(value){
+//       return value.toLocaleString()
+// })
+
+
+
+// var app = new Vue({
+//    el:'#app8',
+//    data:{
+//       jpyPrice: 29800
+//    },
+// })
+
+
+
+var app = new Vue({
+   el: '#app9',
+   data:{
+      url:'https://www.google.co.jp/'
+   }
+})
+
